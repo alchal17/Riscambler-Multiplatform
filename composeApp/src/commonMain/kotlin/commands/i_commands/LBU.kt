66679ae2253632fc97350a01edc5a15447d294d2
ybevz <eugene.bevz06@gmail.com>
2024-08-29 15:34:24 +0300

    Added load ops for unsigned. LD and LWU are omitted due to irrelevance in RV32I

---
+++ composeApp/src/commonMain/kotlin/commands/i_commands/LBU.kt
@@ -0,0 +1,23 @@
+package commands.i_commands
+
+import data_converter.convertToDecimal
+import memory.Memory
+import memory.Registers
+
+// load byte unsigned (1 byte / 8 bits) from memory
+fun LBU(memory: Memory, regs: Registers, operands: Map<String, String>) {
+    val rd = convertToDecimal(operands["rd"]!!)
+    val rs1 = convertToDecimal(operands["rs1"]!!)
+    val imm = convertToDecimal(operands["imm"]!!)
+
+    val dataAddress = regs.fetch(rs1) + imm
+
+    // Fetch one byte from memory
+    val fetchedData = memory.fetchData(dataAddress).toInt() and 0xFF
+
+    // Zero-extend the 8-bit value to 32 bits
+    val zeroExtendedData = fetchedData and 0x000000FF
+
+    // Write the zero-extended value into the register
+    regs.write(rd, zeroExtendedData)
+}
---
+++ composeApp/src/commonMain/kotlin/commands/i_commands/LHU.kt
@@ -0,0 +1,27 @@
+package commands.i_commands
+
+import data_converter.convertToDecimal
+import memory.Memory
+import memory.Registers
+
+// load halfword unsigned (2 bytes / 16 bits) from memory
+fun LHU(memory: Memory, regs: Registers, operands: Map<String, String>) {
+    val rd = convertToDecimal(operands["rd"]!!)
+    val rs1 = convertToDecimal(operands["rs1"]!!)
+    val imm = convertToDecimal(operands["imm"]!!)
+
+    val dataAddress = regs.fetch(rs1) + imm
+
+    // Fetch two bytes from memory
+    val lowerByte = memory.fetchData(dataAddress).toInt() and 0xFF
+    val upperByte = memory.fetchData(dataAddress + 1).toInt() and 0xFF
+
+    // Combine them into a 16-bit value
+    val fetchedData = (upperByte shl 8) or lowerByte
+
+    // Zero-extend the 16-bit value to 32 bits
+    val zeroExtendedData = fetchedData and 0x0000FFFF
+
+    // Write the zero-extended value into the register
+    regs.write(rd, zeroExtendedData)
+}
