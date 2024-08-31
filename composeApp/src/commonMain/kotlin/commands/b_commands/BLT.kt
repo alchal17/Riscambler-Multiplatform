b75a7b011b10b861ebe6191253aee2a12f8a06cd
ybevz <eugene.bevz06@gmail.com>
2024-08-31 17:25:12 +0300

    B commands implemented; pseudo instructions left

---
+++ composeApp/src/commonMain/kotlin/commands/b_commands/BLT.kt
@@ -0,0 +1,15 @@
+package commands.b_commands
+
+import data_converter.convertToDecimal
+import memory.Registers
+
+fun BLT(pc: Int, regs: Registers, operands: Map<String, String>): Int {
+    val rs1 = convertToDecimal(operands["rs1"]!!)
+    val rs2 = convertToDecimal(operands["rs2"]!!)
+    val imm = convertToDecimal(operands["imm"]!!)
+
+    val data1 = regs.fetch(rs1)
+    val data2 = regs.fetch(rs2)
+
+    return if (data1 < data2) pc + imm else pc
+}
